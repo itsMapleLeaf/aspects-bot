6806f20db23174e7e82b2d539cadc6468f8cdd6e
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-02-19 18:12:11 -0600

    roll a single die

--- discord/slash-command.ts
+++ discord/slash-command.ts
@@ -2,33 +2,38 @@ import chalk from "npm:chalk"
 import * as Discord from "npm:discord.js"
 import prettyMs from "npm:pretty-ms"
 import { callTimedAsync } from "../helpers/call-timed.ts"
-import { NonNullableWhen } from "../types.ts"
+import { Awaitable, NonNullableWhen } from "../types.ts"
 
 export type SlashCommand = {
-	data: Discord.ApplicationCommandData
+	options: Record<string, SlashCommandOption>
+	getData: () => Discord.ApplicationCommandData
 	run: (interaction: Discord.ChatInputCommandInteraction) => unknown
 }
 
+type OptionData = Exclude<
+	Discord.ApplicationCommandOptionData,
+	| Discord.ApplicationCommandSubCommandData
+	| Discord.ApplicationCommandSubGroupData
+>
+
 export type SlashCommandOption = {
-	data: Omit<
-		Exclude<
-			Discord.ApplicationCommandOptionData,
-			| Discord.ApplicationCommandSubGroupData
-			| Discord.ApplicationCommandSubCommandData
-		>,
-		"name"
-	>
+	getData: (
+		name: string,
+	) => OptionData
 	getValue: (
 		name: string,
 		interaction: Discord.ChatInputCommandInteraction,
 	) => unknown
+	getAutocompleteChoices?: (
+		interaction: Discord.AutocompleteInteraction,
+	) => Awaitable<Discord.ApplicationCommandOptionChoiceData[]>
 }
 
 type SlashCommandOptionValues<
 	Options extends Record<string, SlashCommandOption>,
 > = {
 	[K in keyof Options]: NonNullableWhen<
-		Options[K]["data"]["required"],
+		ReturnType<Options[K]["getData"]>["required"],
 		Awaited<ReturnType<Options[K]["getValue"]>>
 	>
 }
@@ -41,7 +46,7 @@ export function useSlashCommands(
 		try {
 			const [time] = await callTimedAsync(async function setCommands() {
 				await client.application.commands.set(
-					commands.map((command) => command.data),
+					commands.map((command) => command.getData()),
 				)
 			})
 			console.info(
@@ -57,41 +62,58 @@ export function useSlashCommands(
 	})
 
 	client.on("interactionCreate", async (interaction) => {
-		if (!interaction.isChatInputCommand()) return
-
-		const command = commands.find((c) =>
-			c.data.name === interaction.commandName
-		)
-
-		try {
-			await command?.run(interaction)
-		} catch (error) {
-			console.error(`Failed running command:`, error, {
-				command: command?.data,
-				interaction: interaction.toJSON(),
-			})
+		if (interaction.isChatInputCommand()) {
+			const command = commands.find((c) =>
+				c.getData().name === interaction.commandName
+			)
 
 			try {
-				const options = {
-					content: `Sorry, something went wrong. Try again?`,
-					ephemeral: true,
-				}
-				if (interaction.deferred) {
-					await interaction.editReply(options)
-				} else if (interaction.replied) {
-					await interaction.followUp(options)
-				} else {
-					await interaction.reply(options)
-				}
+				await command?.run(interaction)
 			} catch (error) {
-				console.error(`Failed to reply to interaction:`, error)
+				console.error(`Failed running command:`, error, {
+					command: command?.getData(),
+					interaction: interaction.toJSON(),
+				})
+
+				try {
+					const options = {
+						content: `Sorry, something went wrong. Try again?`,
+						ephemeral: true,
+					}
+					if (interaction.deferred) {
+						await interaction.editReply(options)
+					} else if (interaction.replied) {
+						await interaction.followUp(options)
+					} else {
+						await interaction.reply(options)
+					}
+				} catch (error) {
+					console.error(`Failed to reply to interaction:`, error)
+				}
+			}
+		}
+
+		if (interaction.isAutocomplete()) {
+			const command = commands.find((c) =>
+				c.getData().name === interaction.commandName
+			)
+			if (command) {
+				for (const [name, option] of Object.entries(command.options)) {
+					if (name === interaction.options.getFocused()) {
+						const choices = await option.getAutocompleteChoices?.(interaction)
+						if (choices) {
+							await interaction.respond(choices)
+							continue
+						}
+					}
+				}
 			}
 		}
 	})
 }
 
 export function defineSlashCommand<
-	Options extends Record<string, SlashCommandOption>,
+	const Options extends Record<string, SlashCommandOption>,
 >({ options, run, ...args }: {
 	name: string
 	description: string
@@ -102,17 +124,21 @@ export function defineSlashCommand<
 	) => unknown
 }): SlashCommand {
 	return {
-		data: {
+		options,
+		getData: () => ({
 			...args,
 			options: Object.entries(options).map(([name, option]) =>
-				({ ...option.data, name }) as Discord.ApplicationCommandOptionData
+				({
+					...option.getData(name),
+					name,
+				}) as Discord.ApplicationCommandOptionData
 			),
-		},
+		}),
 		run: async (interaction) => {
 			const values: Record<string, unknown> = {}
 			for (const [name, optionArg] of Object.entries(options)) {
 				const value = await optionArg.getValue(name, interaction)
-				if (optionArg.data.required && value == null) {
+				if (optionArg.getData(name).required && value == null) {
 					throw new Error(`Missing required option: ${name}`)
 				}
 				values[name] = value
@@ -123,7 +149,7 @@ export function defineSlashCommand<
 }
 
 function createOption<
-	const Data extends SlashCommandOption["data"],
+	const Data extends Omit<OptionData, "name">,
 	const Value,
 >(
 	data: Data,
@@ -133,9 +159,14 @@ function createOption<
 	) => Value | null,
 ) {
 	return {
-		data: { ...data, required: true },
+		getData: (name: string) =>
+			({
+				...data,
+				name,
+				required: true,
+			}) as const,
 		getValue,
-	} satisfies SlashCommandOption
+	}
 }
 
 export function optional<const Option extends SlashCommandOption>(
@@ -143,21 +174,40 @@ export function optional<const Option extends SlashCommandOption>(
 ) {
 	return {
 		...option,
-		data: { ...option.data, required: false },
+		getData: (name) => ({ ...option.getData(name), required: false }),
 	} satisfies SlashCommandOption
 }
 
-export function stringOption(description: string) {
+export function stringOption(
+	description: string,
+	choices?: [string, ...string[]],
+) {
 	return createOption({
 		description,
 		type: Discord.ApplicationCommandOptionType.String,
+		choices: choices?.map((value) => ({ name: value, value })),
 	}, (name, interaction) => interaction.options.getString(name))
 }
 
-export function integerOption(description: string) {
+export function numberOption(
+	description: string,
+	choices?: [number, ...number[]],
+) {
+	return createOption({
+		description,
+		type: Discord.ApplicationCommandOptionType.Number,
+		choices: choices?.map((value) => ({ name: value.toString(), value })),
+	}, (name, interaction) => interaction.options.getNumber(name))
+}
+
+export function integerOption(
+	description: string,
+	choices?: [number, ...number[]],
+) {
 	return createOption({
 		description,
 		type: Discord.ApplicationCommandOptionType.Integer,
+		choices: choices?.map((value) => ({ name: value.toString(), value })),
 	}, (name, interaction) => interaction.options.getInteger(name))
 }
 
--- main.ts
+++ main.ts
@@ -1,7 +1,9 @@
 import "https://deno.land/x/dotenv@v3.2.2/load.ts"
 import * as Discord from "npm:discord.js"
 import {
+	booleanOption,
 	defineSlashCommand,
+	optional,
 	stringOption,
 	useSlashCommands,
 } from "./discord/slash-command.ts"
@@ -23,6 +25,29 @@ useSlashCommands(client, [
 		},
 		run: async (options, interaction) => {
 			await interaction.reply(options.input)
+		},
+	}),
+	defineSlashCommand({
+		name: "roll",
+		description: "Roll some dice",
+		options: {
+			die: stringOption("The die to roll", [
+				"1d4",
+				"1d6",
+				"1d8",
+				"1d12",
+				"1d20",
+			]),
+			private: optional(booleanOption("Only you can see this.")),
+		},
+		run: async (options, interaction) => {
+			const result = Math.floor(
+				Math.random() * parseInt(options.die.split("d")[1]) + 1,
+			)
+			await interaction.reply({
+				content: `🎲 ${options.die} -> **${result}**`,
+				ephemeral: options.private ?? false,
+			})
 		},
 	}),
 ])
