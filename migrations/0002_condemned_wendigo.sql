5a014439ac675070186828f0772e9186c27d5fd3
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-02-21 03:28:04 -0600

    let's not bother with migrations lol

--- migrations/0002_condemned_wendigo.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `aspectSkillsToAspects` (
-	`aspectSkillId` text NOT NULL,
-	`aspectId` text NOT NULL,
-	FOREIGN KEY (`aspectSkillId`) REFERENCES `aspectSkills`(`id`) ON UPDATE no action ON DELETE no action,
-	FOREIGN KEY (`aspectId`) REFERENCES `aspects`(`id`) ON UPDATE no action ON DELETE no action
-);
---> statement-breakpoint
-ALTER TABLE `aspectSkills` DROP COLUMN `aspectIds`;
