201ca0c95748244a535f30a18cfc53b2c60b6fdc
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-02-22 14:04:47 -0600

    remove diceKinds map

--- src/game-data.ts
+++ src/game-data.ts
@@ -7,16 +7,6 @@ import { raise } from "./helpers/errors.ts"
 import { Logger } from "./logger.ts"
 import * as schema from "./schema.ts"
 
-export const diceKinds = new Map(
-	Object.entries({
-		d4: { id: "d4", name: "d4", faces: 4 },
-		d6: { id: "d6", name: "d6", faces: 6 },
-		d8: { id: "d8", name: "d8", faces: 8 },
-		d12: { id: "d12", name: "d12", faces: 12 },
-		d20: { id: "d20", name: "d20", faces: 20 },
-	}),
-)
-
 const notion = new Notion.Client({
 	auth: env.NOTION_API_KEY,
 })
