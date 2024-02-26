50bbe0d3eda32f85b59d5999868f00be44cbdc12
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2024-02-25 21:43:02 -0600

    i love the using keyword

--- src/hooks/context.ts
+++ src/hooks/context.ts
@@ -4,18 +4,23 @@ export function createContext<Value, ProvideArgs>(
 	init: (args: ProvideArgs) => Value,
 ) {
 	let current: Value | typeof empty = empty
-	return {
-		provide(args: ProvideArgs, block: () => void) {
-			const previous = current
-			current = init(args)
-			block()
-			current = previous
-		},
-		use() {
-			if (current === empty) {
-				throw new Error("No context provider")
-			}
-			return current
-		},
+
+	function provide(args: ProvideArgs) {
+		const previous = current
+		current = init(args)
+		return {
+			[Symbol.dispose]() {
+				current = previous
+			},
+		}
 	}
+
+	function use() {
+		if (current === empty) {
+			throw new Error("Context not provided")
+		}
+		return current
+	}
+
+	return { provide, use }
 }
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { useRollCommands } from "./dice/useRollCommands.ts"
 import { useCommands } from "./discord/command-handler.ts"
 import { SlashCommandContext } from "./discord/commands/SlashCommandContext.ts"
 import { useInteractionHandlers } from "./discord/interactions/InteractionHandler.ts"
+import { MessageComponentContext } from "./discord/messageComponents/MessageComponentContext.ts"
 import { env } from "./env.ts"
 import { Logger } from "./logger.ts"
 
@@ -21,9 +22,11 @@ client.on("ready", (client) => {
 	Logger.info((f) => `Logged in as ${f.highlight(client.user?.tag)}`)
 })
 
-SlashCommandContext.provide(client, () => {
+{
+	using _slashCommands = SlashCommandContext.provide(client)
+	using _messageComponents = MessageComponentContext.provide(client)
 	useRollCommands()
-})
+}
 
 useCommands(client, [...characterCommands, ...combatCommands])
 useInteractionHandlers(client, [combatSetup, combatTracker])
